d45d7de64cc0739564be1a8be4af3b5e4cbb91f1
Angel C. <1001.31118017.ucla@gmail.com>
2024-10-04 23:43:51 -0400

    Cambios en las vistas Cine y Familia

--- JS/Cl_vCine.js
+++ JS/Cl_vCine.js
@@ -1,7 +1,20 @@
 export class Cl_vCine { 
-    reportar( salida, devolverAuxNombre, promLunes,totalCaja) { 
-    salida.innerHTML += `<br>Familia que pagó más: ${devolverAuxNombre}`;
-    salida.innerHTML += `<br>Promedio ppagado los días lunes: ${totalCaja}`;
-    salida.innerHTML += `<br>Total de dinero al final de la jornada: ${promLunes}`;
-  }
+constructor (controlador){
+this.vista = document.getElementById("mainform");
+this.tabla = document.getElementById("mainform_table");
+this.lblFamilia = document.getElementById("mainform_lblFamilia");
+this.lblPromedio = document.getElementById("main_lblPromedio");
+this.lblTotal = document.getElementById("mainform_lblTotal");
+this.btAgregar = document.getElementById("mainform.btAgregar");
+this.btAgregar.onclick = () => controlador.mostrarVFamilia();
+}
+
+  mostrar(){
+    this.vista.hidden = false;
+    }
+
+  ocultar(){
+    this.vista.hidden = true;
+    }
+
 } 
--- JS/Cl_vFamilia.js
+++ JS/Cl_vFamilia.js
@@ -1,11 +1,27 @@
-export class Cl_vFamilia { 
-    leerDatos() {
-        let familia = prompt("familia");
-        let personas = prompt("personas");
-        let dia = prompt("dia");
-        return { familia, personas, dia }; 
-    } 
-    reportar (salida, familia, montoPagar) { 
-        salida.innerHTML += `<br>La familia ${familia} tiene que pagar ${montoPagar}`;
-    } 
+export class Cl_vFamilia { //prefijo "in" es por input
+    
+  constructor(controlador){
+    this.vista = document.getElementById("familyform");
+    this.infamilia = document.getElementById("familyform_familia");
+    this.inpersonas = document.getElementById("familyform_personas");
+    this.india = document.getElementById("familyform_dia");
+    this.btAceptar = document.getElementById("familyform_btAceptar");
+    this.btAceptar.onclick = () =>
+    
+    controlador.agregarfamilia({
+        familia = this.infamilia.value,
+        personas = this.inpersonas.value,
+        dia = this.india.value,
+    });
+        this.ocultar();
+
+    }
+     
+mostrar(){
+    this.vista.hidden = false;
+    }
+
+ocultar(){
+    this.vista.hidden = true;
+    }
 }
