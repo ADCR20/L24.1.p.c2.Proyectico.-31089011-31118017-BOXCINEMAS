3bbe0fb0d312ae1a4616c767f5a88121df27e0ba
Angel C. <1001.31118017.ucla@gmail.com>
2024-10-04 23:52:38 -0400

    Cambio en el vCine

--- JS/Cl_vCine.js
+++ JS/Cl_vCine.js
@@ -8,6 +8,19 @@ this.lblTotal = document.getElementById("mainform_lblTotal");
 this.btAgregar = document.getElementById("mainform.btAgregar");
 this.btAgregar.onclick = () => controlador.mostrarVFamilia();
 }
+//verificar reportarCine
+reportarCine((acumlunes, contlunes, mayor, auxfamilia, acumtotal, dinero)) {
+  this.tabla.innerHTML += `
+  <tr>
+  <td>${Familia}</td>
+  <td>${Personas}</td>
+  <td>${Día}</td>
+  <td>${Monto a pagar}</td>
+  </tr>`;
+
+  this.lblFamilia.innerHTML = ;
+  this.lblPromdeio.innerHTML = ;
+  this.lbltotal.innerHTML =;
 
   mostrar(){
     this.vista.hidden = false;
